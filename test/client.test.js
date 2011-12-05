cc1171df9ce178c44d602f237147d17f52c4bdee
Chris Sainty <chris.sainty@quids.com.au>
2011-12-05 22:38:33 +1100

    Fleshing out client object with functions for
    constructQuery()
    getDatabaseNames()
    getDocument()
    putDocument()
    ensureDatabaseExists()

--- test/client.test.js
+++ test/client.test.js
@@ -1,8 +1,57 @@
+var util = require('util');
+var should = require('should');
 var raven = require('../lib/client');
 var server = raven('http://localhost:8080');
+var databaseNames;
 
 describe('Client', function() {
-	describe('#queryIndex()', function(){
+	describe('constructQuery()', function() {
+		it('should prepend server name to queries', function(){
+			server.constructQuery('/databases').should.equal('http://localhost:8080/databases');
+		})
+	})
+
+	describe('getDatabaseNames()', function() {
+		it('should return an array of database names', function(done){
+			server.getDatabaseNames(function (result){
+				should.exist(result);
+				result.should.be.an.instanceof(Array);
+				databaseNames = result;
+				done();
+			})
+		})
+	})
+	
+	describe('putdocument()', function() {
+		it('should return true when saving a document', function (done) {
+			server.putDocument('docs-1', { 'message': 'Testing.1.2.3' }, function(result){
+				should.exist(result);
+				result.should.be.true;
+				done();
+			})
+		})
+	})
+	
+	describe('getDocument()', function(){
+		it('should return null if document is not found', function (done) {
+			server.getDocument('invalidKey', function(result) {
+				should.not.exist(result);
+				done();
+			});
+		})
+		it('should return the correct document')
+	})
+	
+	describe('ensureDatabaseExists()', function() {
+		it('should create a database that does not exist', function(done){
+			server.ensureDatabaseExists('node-raven', done);
+		})
+		it('should not error when a database already exists', function(done){
+			server.ensureDatabaseExists('node-raven', done);
+		})
+	})
+
+	describe('queryIndex()', function(){
 		it('should return results', function(done) {
 			server.queryIndex('test', '', function(result) {
 				result.should.equal('test');
