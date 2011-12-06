04d969b5182df114bb32f6698f6b3a14e4ccc7b5
Chris Sainty <chris.sainty@quids.com.au>
2011-12-06 20:21:32 +1100

    Extended the wait for server to start up.
    Need to update the test script to get it to poll until the server is up so that we do not need longer than necessary.

--- test.cmd
+++ test.cmd
@@ -1,6 +1,6 @@
 @echo off
 del /q /s .\Ravendb\data > NUL
 start "node-raven-test-server" /MIN .\RavenDb\Raven.Server
-timeout /T 3 /nobreak > NUL
+timeout /T 5 /nobreak > NUL
 cmd /C .\node_modules\.bin\mocha .\test\client.test.js
 taskkill /F /FI "WINDOWTITLE eq node-raven-test-server" > NUL
