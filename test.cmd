7ec39edcc4f584b0dce10c3c459429983d38c600
Chris Sainty <chris.sainty@quids.com.au>
2011-12-09 16:45:11 +1100

    Removed timeout from test script, now calls a node script that polls the server until it is up.
    It is a fairly heavy poll, but that keeps it simple.

--- test.cmd
+++ test.cmd
@@ -1,7 +1,7 @@
 @echo off
 del /q /s .\Ravendb\data > NUL
 start "node-raven-test-server" /MIN .\RavenDb\Raven.Server
-timeout /T 5 /nobreak > NUL
+node .\test\waitforserver.js
 cmd /C .\node_modules\.bin\mocha .\test\ravenhttpclient.test.js .\test\client.test.js 
 taskkill /F /FI "WINDOWTITLE eq node-raven-test-server" > NUL
 
---
+++ test/waitforserver.js
@@ -0,0 +1,17 @@
+var server_url = 'http://localhost:8080';
+var request = require('request');
+
+console.log('Waiting for RavenDb server @ ' + server_url + ' to start responding');
+checkServerIsUp();
+
+function checkServerIsUp() {
+	request(server_url + '/build/version', function (error, response) {
+		if (!error && response.statusCode == 200) {
+			return;
+		} 
+		checkServerIsUp();
+	});
+}
+
+
+
