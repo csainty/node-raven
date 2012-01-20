d9901586fdfaad164a45efd24d738b1b9cfe8428
Chris Sainty <chris.sainty@quids.com.au>
2012-01-21 00:01:28 +1100

    Fix for sample script to get it to wait for the server to load before starting up

--- sample.cmd
+++ sample.cmd
@@ -1,6 +1,7 @@
 @echo off
 start "node-raven-sample-server" /MIN .\RavenDb\Raven.Server
 
+node .\test\waitforserver.js
 node .\sample\app.js	
 
 taskkill /F /FI "WINDOWTITLE eq node-raven-sample-server" > NUL
