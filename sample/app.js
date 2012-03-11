ab787e318129571bcf2bd76eb863e18dd9ea0fbd
Chris Sainty <chris.sainty@quids.com.au>
2012-03-11 13:25:07 +1100

    Improved sample app startup process.

--- sample/app.js
+++ sample/app.js
@@ -6,8 +6,33 @@
 var express = require('express'),
 	routes = require('./routes'),
 	raven = require('../lib/client.js'),
-	server = raven({ server_url : 'http://localhost:8080/' }),
-	app = module.exports = express.createServer();
+	server = raven({ server_url : 'http://localhost:8080/' });
+
+var app = module.exports = express.createServer();
+
+// Configuration
+app.configure(function () {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function () {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+	app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+// Connect to RavenDb and create our database if needed
 
 server.ensureDatabaseExists('node-raven-sample', function (error, response, ok) {
 	if (error || !ok) {
@@ -16,33 +41,11 @@ server.ensureDatabaseExists('node-raven-sample', function (error, response, ok) 
 
 	server.useDatabase('node-raven-sample');
 
-	// Configuration
-	app.configure(function () {
-		app.set('views', __dirname + '/views');
-		app.set('view engine', 'jade');
-		app.use(express.bodyParser());
-		app.use(express.methodOverride());
-		app.use(app.router);
-		app.use(express.static(__dirname + '/public'));
-	});
-
-	app.configure('development', function () {
-		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-	});
-
-	app.configure('production', function () {
-		app.use(express.errorHandler());
-	});
-
-	// Routes
-
-	app.get('/', routes.index);
-
+	// Start the server listening
 	app.listen(3000);
 	console.log("The node-raven sample application is listening on port %d in %s mode. Type 'exit' and hit <ENTER> to quit.", app.address().port, app.settings.env);
 
-
-	// The below code is just to enable the exit by typeing quit functionality. It is not generally needed in production
+	// The below code is just to enable the exit functionality. It is not generally needed in production
 	process.stdin.resume();
 	process.stdin.setEncoding('utf8');
 	process.stdin.on('data', function (chunk) {
