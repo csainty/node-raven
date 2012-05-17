b275ce851226c9dfc88d515b62789a493b268e72
Chris Sainty <csainty@hotmail.com>
2012-05-17 13:01:53 +1000

    Missed a file in the last commit. Again just indentation.

--- utils/waitforserver.js
+++ utils/waitforserver.js
@@ -5,10 +5,10 @@ console.log('Waiting for RavenDb server @ ' + server_url + ' to start responding
 checkServerIsUp();
 
 function checkServerIsUp() {
-	request(server_url + '/build/version', function (error, response) {
-		if (!error && response.statusCode == 200) {
-			return;
-		} 
-		checkServerIsUp();
-	});
+  request(server_url + '/build/version', function (error, response) {
+    if (!error && response.statusCode == 200) {
+      return;
+    } 
+    checkServerIsUp();
+  });
 }
